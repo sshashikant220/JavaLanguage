7e666889cd987ff7ce3fe90bac1c4689f5516e29
SHASHIKANT SINGH <79221089+sshashikant220@users.noreply.github.com>
2025-03-31 19:27:04 +0530

    Basic Java Program

---
+++ basic java Program/demo.java
@@ -0,0 +1,19 @@
+class Demo {
+
+    public static void main(String args[]) {
+
+        int age = 88;
+
+        if (age >= 18 && age <= 60) {
+
+            System.out.println("You are adult ");
+
+        } else if (age > 60) {
+            System.out.println("You are Senier Citizen");
+
+        } else {
+            System.out.println("You are Minor ");
+        }
+
+    }
+}
